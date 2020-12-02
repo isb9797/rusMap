359e507c26d3eb15a46bb517dc5ca11c7ed991e3
Sergey <isb9797@gmail.com>
2020-12-02 20:43:09 +0300

    Added Data Base JSON and first version Request Functions

--- app/js/app.js
+++ app/js/app.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     start() {
       //Получаем список регионов
-      this.mapRegions = document.querySelectorAll(".region");
+      this.mapRegions = document.querySelectorAll(".region_item");
 
       const regions = this.mapRegions;
 
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
             {
               y: cy - this.modifyVar,
               fill: "#0090FF",
-              zIndex: 10000,
+              filter: "drop-shadow( 0px 20px 4px #000 )",
               duration: 0.25,
             }
           );
@@ -46,16 +46,60 @@ document.addEventListener("DOMContentLoaded", () => {
 
             {
               y: cy,
-              zIndex: 0,
               fill: "#dededf",
+              filter: "drop-shadow( 0px 0px 0px #dededf)",
               duration: 0.25,
             }
           );
         });
       });
     }
+
+    requestToServer(id) {
+      let qty_product = 2;
+
+      // Первым аргументом кладем путь, + строку как и в любом другом запросе, ключ=значение&ключ=значение
+      fetch(
+        "../db.json",
+
+        // Второй аргумент это объект с указаниями, метода и заголовка
+        {
+          method: "GET",
+          headers: { "content-type": "application/x-www-form-urlencoded" },
+        }
+      )
+        .then((response) => {
+          if (response.status !== 200) {
+            return Promise.reject();
+          }
+          return response.text();
+        })
+        .then((i) => {
+          const response = JSON.parse(i);
+          const infoBlock = document.querySelector(".information");
+          const regions = document.querySelectorAll("g .region");
+
+          regions.forEach((item, index) => {
+            item.addEventListener("click", (e) => {
+              infoBlock.innerHTML = "";
+              for (let resitem in response) {
+                if (item.id === resitem) {
+                  infoBlock.insertAdjacentHTML(
+                    "afterBegin",
+                    `<h1>${response[id].header}</h1>
+                    <p>${response[id].description}</p>
+                    <a href="#" class="btn button">Подробнее о регионе</a>`
+                  );
+                }
+              }
+            });
+          });
+        })
+        .catch(() => console.log("ошибка"));
+    }
   }
 
   const map = new RussianMap();
   map.start();
+  map.requestToServer("KYA");
 });
