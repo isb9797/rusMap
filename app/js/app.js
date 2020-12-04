26ed707862fec1b9ee722a40f8b6b2281bbca919
Sergey <isb9797@gmail.com>
2020-12-04 18:39:19 +0300

    Added Cursor and animation

--- app/js/app.js
+++ app/js/app.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     start() {
       //Получаем список регионов
-      this.mapRegions = document.querySelectorAll(".region_item");
+      this.mapRegions = document.querySelectorAll(".active, .region_item");
 
       const regions = this.mapRegions;
 
@@ -56,8 +56,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     requestToServer(id) {
-      let qty_product = 2;
-
       // Первым аргументом кладем путь, + строку как и в любом другом запросе, ключ=значение&ключ=значение
       fetch(
         "../db.json",
@@ -82,24 +80,161 @@ document.addEventListener("DOMContentLoaded", () => {
           regions.forEach((item, index) => {
             item.addEventListener("click", (e) => {
               infoBlock.innerHTML = "";
+              gsap.to(
+                infoBlock,
+                0,
+
+                {
+                  opacity: 0,
+                  duration: 0,
+                }
+              );
+
               for (let resitem in response) {
                 if (item.id === resitem) {
+                  console.log(item.id);
+                  infoBlock.innerHTML = "";
                   infoBlock.insertAdjacentHTML(
                     "afterBegin",
-                    `<h1>${response[id].header}</h1>
-                    <p>${response[id].description}</p>
+                    `<h1>${response[resitem].header}</h1>
+                    <p>${response[resitem].description}</p>
                     <a href="#" class="btn button">Подробнее о регионе</a>`
                   );
                 }
+
+                gsap.to(
+                  infoBlock,
+                  1,
+
+                  {
+                    opacity: 1,
+                    duration: 0.5,
+                  }
+                );
               }
             });
           });
         })
         .catch(() => console.log("ошибка"));
     }
+
+    cursorInit() {
+      // Custom Cursor
+      var cursor = {
+        delay: 8,
+        _x: 0,
+        _y: 0,
+        endX: window.innerWidth / 2,
+        endY: window.innerHeight / 2,
+        cursorVisible: true,
+        cursorEnlarged: false,
+        $dot: document.querySelector(".ppk-dot"),
+        $outline: document.querySelector(".ppk-dot-outline"),
+
+        init: function () {
+          // Set up element sizes
+          this.dotSize = this.$dot.offsetWidth;
+          this.outlineSize = this.$outline.offsetWidth;
+
+          this.setupEventListeners();
+          this.animateDotOutline();
+        },
+
+        setupEventListeners: function () {
+          var self = this;
+
+          // Anchor hovering
+          document.querySelectorAll("a").forEach(function (el) {
+            el.addEventListener("mouseover", function () {
+              self.cursorEnlarged = true;
+              self.toggleCursorSize();
+            });
+            el.addEventListener("mouseout", function () {
+              self.cursorEnlarged = false;
+              self.toggleCursorSize();
+            });
+          });
+
+          // Click events
+          document.addEventListener("mousedown", function () {
+            self.cursorEnlarged = true;
+            self.toggleCursorSize();
+          });
+          document.addEventListener("mouseup", function () {
+            self.cursorEnlarged = false;
+            self.toggleCursorSize();
+          });
+
+          document.addEventListener("mousemove", function (e) {
+            // Show the cursor
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+
+            // Position the dot
+            self.endX = e.pageX;
+            self.endY = e.pageY;
+            self.$dot.style.top = self.endY + "px";
+            self.$dot.style.left = self.endX + "px";
+          });
+
+          // Hide/show cursor
+          document.addEventListener("mouseenter", function (e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$dot.style.opacity = 1;
+            self.$outline.style.opacity = 1;
+          });
+
+          document.addEventListener("mouseleave", function (e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$dot.style.opacity = 0;
+            self.$outline.style.opacity = 0;
+          });
+        },
+
+        animateDotOutline: function () {
+          var self = this;
+
+          self._x += (self.endX - self._x) / self.delay;
+          self._y += (self.endY - self._y) / self.delay;
+          self.$outline.style.top = self._y + "px";
+          self.$outline.style.left = self._x + "px";
+
+          requestAnimationFrame(this.animateDotOutline.bind(self));
+        },
+
+        toggleCursorSize: function () {
+          var self = this;
+
+          if (self.cursorEnlarged) {
+            self.$dot.style.transform = "translate(-50%, -50%) scale(0.75)";
+            self.$outline.style.transform = "translate(-50%, -50%) scale(1.5)";
+          } else {
+            self.$dot.style.transform = "translate(-50%, -50%) scale(1)";
+            self.$outline.style.transform = "translate(-50%, -50%) scale(1)";
+          }
+        },
+
+        toggleCursorVisibility: function () {
+          var self = this;
+
+          if (self.cursorVisible) {
+            self.$dot.style.opacity = 1;
+            self.$outline.style.opacity = 1;
+          } else {
+            self.$dot.style.opacity = 0;
+            self.$outline.style.opacity = 0;
+          }
+        },
+      };
+
+      cursor.init();
+    }
   }
 
   const map = new RussianMap();
   map.start();
-  map.requestToServer("KYA");
+  map.requestToServer();
+  map.cursorInit();
 });
